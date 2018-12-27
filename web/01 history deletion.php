1e13f642555a91290fd71f63729ea0ae68f1581f
Jaroslav Týc <mail@jaroslavtyc.com>
2018-12-27 09:23:22 +0100

    Testing content for calculator skeleton

---
+++ web/01 history deletion.php
@@ -0,0 +1,4 @@
+<?php
+/** @var \DrdPlus\CalculatorSkeleton\CalculatorController $controller */
+/** @noinspection PhpIncludeInspection */
+include $controller->getDirs()->getGenericPartsRoot() . '/history_deletion.php';
